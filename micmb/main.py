a5555830650a00958ea0a821dd572d158a39b1bf
Luzi82 <luzi82@gmail.com>
2018-04-16 00:34:34 +0800

    imporve detection

--- micmb/main.py
+++ micmb/main.py
@@ -353,7 +353,7 @@ for write_domain in domain_list:
                 'unknown' if data.get_max_domain_try_write_time(write_domain) < timestamp - config['timeout'] else \
                 'unknown' if data.get_min_domain_try_write_time(write_domain) >= timestamp - config['min_deliver_time'] else \
                 'unknown' if data.get_domain2_follow_time(write_domain,read_domain) is None else \
-                'unknown' if data.get_domain_try_read_time(read_domain) - data.get_domain2_follow_time(write_domain,read_domain) < config['min_deliver_time'] else \
+                'unknown' if data.get_domain2_follow_time(write_domain,read_domain) >= timestamp - config['timeout'] else \
                 'bad'
         state_data_dict[key] = state
 
